2c74a800c294c507cfaf90e0b745e51d6565d0c6
Lars Lippert Ovesen <lars_lippert@hotmail.com>
2021-02-23 15:27:45 +0100

    Added cpu and memory limits

--- README.md
+++ README.md
@@ -14,6 +14,12 @@ $ kubectl apply -f ./deployment.yaml
 $ kubectl apply -f ./service.yaml
 $ minikube service --url cfc-service
 
+# To run again
+$ docker build -t <dockerID>/cfc .
+$ minikube start
+$ minikube service --url cfc-service
+
 # To delete stuff
 $ kubectl delete -f ./deployment.yaml
 $ kubectl delete -f ./service.yaml
+Delete rep in dockerhub
---
+++ test/stressTest.ps1
@@ -0,0 +1,16 @@
+﻿$responseTime = New-Object Collections.Generic.List[Int]
+$url = "http://127.0.0.1:54528/cookies" # Needs to be set
+
+$len = 1000
+
+for ($i = 1; $i -lt $len; $i++) {    $r = (Measure-Command -Expression { $site = Invoke-WebRequest -Uri $url -UseBasicParsing }).Milliseconds
+
+    $responseTime.Add($r)
+}
+
+$sum = 0
+$responseTime|Foreach{$sum += $_}
+
+$avg = $sum/$len
+
+$avg
